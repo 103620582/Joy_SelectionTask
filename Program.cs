6d0249a4c3222537028ef02732dbd97894de8036
103620582 <103620582@student.swin.edu.au>
2021-03-18 19:20:04 +1100

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+	
+        static void Main(string[] args) {
+		
+			int age;
+			string gender;
+			
+			Console.WriteLine("Gday! Please enter your age.");
+			age = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Please enter your gender: ");
+			gender = (Console.ReadLine());
+
+	// true branch
+			if (age >= 17)
+			{
+			if (gender == "M")
+			
+				Console.WriteLine("Nice! You're legally an adult Male.");
+			} 
+			else {
+			if (age >= 17)
+			{
+			if (gender == "F")
+				Console.WriteLine("Nice! You're legally an adult Female.");
+			}
+
+	// false branch
+			else {
+			if (age < 17) 
+				{
+				Console.WriteLine("Hmm. Looks like you're legally not an adult");
+				}
+			}
+        }
+	
