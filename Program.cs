42bfa6806db92d472834378b09b938e1a6e2f6ec
103620582 <103620582@student.swin.edu.au>
2021-03-25 17:38:19 +1100

    Exercise 1 final submission

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
 
 namespace SelectionTask
 {
-    class Program
-    {
+    class Program 
+	{
 	
-        static void Main(string[] args) {
+        static void Main(string[] args) 
+		{
 		
 			int age;
 			string gender;
@@ -13,29 +14,51 @@ namespace SelectionTask
 			Console.WriteLine("Gday! Please enter your age.");
 			age = int.Parse(Console.ReadLine());
 
-			Console.WriteLine("Please enter your gender: ");
+			Console.WriteLine("Please enter your gender.");
 			gender = (Console.ReadLine());
 
-	// true branch
+			// true branch
 			if (age >= 17)
 			{
-			if (gender == "M")
-			
-				Console.WriteLine("Nice! You're legally an adult Male.");
-			} 
-			else {
-			if (age >= 17)
-			{
-			if (gender == "F")
-				Console.WriteLine("Nice! You're legally an adult Female.");
-			}
-
-	// false branch
-			else {
-			if (age < 17) 
+				if (gender == "M")
 				{
-				Console.WriteLine("Hmm. Looks like you're legally not an adult");
+					Console.WriteLine("Nice! You're legally an adult Male.");
+				}
+			
+				else {
+					if (age >= 17)
+					{
+						if (gender == "F") {
+							Console.WriteLine("Nice! You're legally an adult Female.");
+						}
+					}
 				}
 			}
-        }
-	
+			else {
+				if (age < 17) 
+				{
+					Console.WriteLine("Hmm. Looks like you're legally not an adult");
+				}
+			}
+
+
+			Console.WriteLine("Please enter a number.");
+			string input1 = Console.ReadLine();
+			int num1 = int.Parse(input1);
+
+			Console.WriteLine("Please enter another number.");
+			string input2 = Console.ReadLine();
+			int num2 = int.Parse(input2);
+
+			Console.WriteLine(num1+num2 + "" + "is the total number");
+		
+
+
+		
+
+
+
+		}
+	}
+}
+			
